f881501ba9550fb5eb9aaca20341cdba77a7dff7
Abhijit <abhijit.bansode@learnq.ai>
2022-06-22 17:02:03 +0530

    initial commint

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
-
+import { Unity, useUnityContext } from "react-unity-webgl";
+import "./App.css";
 function App() {
+  const { unityProvider, isLoaded } = useUnityContext({
+    loaderUrl:
+      "games/react-unity-game-build/Build/react-unity-game-build.loader.js",
+    dataUrl: "games/react-unity-game-build/Build/react-unity-game-build.data",
+    frameworkUrl:
+      "games/react-unity-game-build/Build/react-unity-game-build.framework.js",
+    codeUrl: "games/react-unity-game-build/Build/react-unity-game-build.wasm",
+  });
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="game-container">
+      {isLoaded ? (
+        <h3 className="heading-text">React Unity Sample 😎</h3>
+      ) : (
+        <h3 className="heading-text">Please wait while the game is being loaded..</h3>
+      )}
+      <Unity
+        style={{ height: 500, width: 900 }}
+        unityProvider={unityProvider}
+      />
     </div>
   );
 }
